7ef132d1ce635560a01534b3b8ed2d85a103e59a
madhujitha sithruwan <madhujithasithruwan@gmail.com>
2024-02-25 22:50:20 +0530

    Authentication done

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='w-full flex justify-center items-center'>Header</div>
+  )
+}
+
+export default Header
---
+++ src/config/firebase.config.js
@@ -0,0 +1,29 @@
+
+import {getAuth} from 'firebase/auth';
+import { initializeApp,getApp,getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+const firebaseConfig = {
+
+    apiKey: import.meta.env.VITE_APP_API_KEY,
+  
+    authDomain:  import.meta.env.VITE_APP_AUTH_DOMAIN,
+  
+    projectId:  import.meta.env.VITE_APP_PROJECT_ID,
+  
+    storageBucket:  import.meta.env.VITE_APP_STORAGEBUCKET,
+  
+    messagingSenderId:  import.meta.env.VITE_APP_MESSAGING_SENDER_ID,
+  
+    appId:  import.meta.env.VITE_APP_APPID,
+  
+    measurementId: import.meta.env.VITE_APP_MEASUREMENTID,
+  
+  };
+
+  const app = getApps.length > 0 ? getApp(): initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+  const db = getFirestore(app);
+
+  export {auth,db,app};
